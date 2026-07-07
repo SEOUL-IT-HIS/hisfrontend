7ff61c3cf5016d17161472750ccd5508442fad52
jinki238 <jinki238@gmail.com>
2026-07-07 16:44:02 +0900

    테스트중

---
+++ src/app/jinki/page.tsx
@@ -0,0 +1,11 @@
+const JinkiPage = () => {
+  return (
+    <div>
+      <h1>Jinki Page</h1>
+      <p>This is the Jinki page.</p>
+      <p>프론트테스트중</p>
+    </div>
+  );
+};
+
+export default JinkiPage;
