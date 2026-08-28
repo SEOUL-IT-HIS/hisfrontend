000c5e1ab3298d87ca20bcd372623509518e4f09
DESKTOP-HO27USQ\PC <ghddpwls3994@gmail.com>
2026-08-28 11:02:43 +0900

    CB2-19 환자 상세 사망정보 영역 잘림 개선 기능수정

--- src/components/patient/PatientDetailForm.tsx
+++ src/components/patient/PatientDetailForm.tsx
@@ -241,7 +241,7 @@ export default function PatientDetailForm({
   };
 
   return (
-    <div className="flex h-full min-h-0 flex-col gap-3 p-3">
+    <div className="flex min-h-full flex-col gap-3 p-3">
       <PageHeader
         title="환자 상세"
         actions={
